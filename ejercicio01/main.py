55f46d6a7a1a7debe3fa24b11b9342b86bd04c4c
Joao Ventura <goncalvesjventura@gmail.com>
2026-04-22 18:22:44 +0200

    ejercicio01 programa basico python completado

---
+++ ejercicio01/main.py
@@ -0,0 +1,16 @@
+#Ejercicio 01 - Programa basico  de Python
+
+#Solicitar datos al usuario
+
+nombre = input("Introduce tu nombre: ")
+primer_apellido = input ("Introduce tu primer apellido: ")
+segundo_apellido = input (" introduce tu segundo apellido: ")
+edad = input (" Introduce tu edad: ")
+codigo_postal = input (" Introduce tu codigo postal: ")
+poblacion = input (" Introduce tu población: ")
+telefono = input (" Introduce tu teléfono:" )
+
+
+# Mostrar resultados
+print ("Hola " + nombre + " " + primer_apellido + " " + segundo_apellido    + ", tienes " + edad + " años, vives en " + poblacion + " con el código postal " + codigo_postal + " y tu número de teléfono es " + telefono     )
+print ("Gracias por usar el programa, hasta pronto! GRUPO COLON 2026")
