f56ffff56112b12c9c8b7704bee54192e48ef502
Joao Ventura <goncalvesjventura@gmail.com>
2026-05-04 20:23:09 +0200

    x

---
+++ ejercicio11_fibonacci_for/main.py
@@ -0,0 +1,10 @@
+import os
+
+os.system("cls")
+
+import random
+
+numero = []
+
+
+n = random.randint(-100, 100)
--- ejercicio12_array/ejercicio12_array_busqueda/main.py
+++ ejercicio12_array/ejercicio12_array_busqueda/main.py
@@ -1,10 +1,16 @@
 import os
-
 os.system("cls")
 
 import random
 
 numero = []
 
+while len(numero) < 10:
+    n = random.randint(-100, 100)
+    if n != 0 and n not in numero:
+        numero.append(n)
 
-n = random.randint(-100, 100)
+mayor = numero[0]
+menor = numero[0]
+
+forn n 
