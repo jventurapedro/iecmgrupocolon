37608fcd9bb9ef8416e4fed1797035bd174b6cbb
jventurapedro <goncalvesjventura@gmail.com>
2026-05-25 18:30:01 +0200

    Actualizacion importacion MySQL CSV

---
+++ ejemploBD/ejemplopython/crear_importar_tabla.py
@@ -0,0 +1,39 @@
+
+import mysql.connector
+
+conexion1=mysql.connector.connect(host="localhost", user="root", passwd="", db="", allow_local_infile=True)
+cursor1=conexion1.cursor()
+
+cursor1.execute("drop database if exists coches;")
+cursor1.execute("create database coches character set latin1 collate latin1_spanish_ci;")
+cursor1.execute("use coches;")
+cursor1.execute("create table datos (marca varchar(10), modelo varchar(10), anyo varchar(4), color varchar(10), matricula varchar(7) PRIMARY KEY);")
+cursor1.execute("insert into datos values ('Suzuki','Jimny', 2020, 'Gris','0547NHB');")
+cursor1.execute("LOAD DATA INFILE 'C:/Users/jpedr/Desktop/iecmgrupocolon/ejemploBD/ejemplopython/base_coches.csv' INTO TABLE datos FIELDS TERMINATED BY  '\t' LINES TERMINATED BY '\n'")
+
+conexion1.commit()
+
+cursor1.execute("select * from datos;")
+
+tabla = list()
+
+
+
+for fila in cursor1:
+    tabla.append(fila)
+
+longitud = len(tabla)
+ancho = len(tabla[0])
+
+
+for i in range(longitud):
+    linea = ""
+    for j in range(ancho):
+        linea = linea + str(tabla[i][j]) + "\t"
+    print(linea)
+
+
+print("\nEn la tabla datos tienes " + str(ancho)+ " campos y " + str(longitud) + " registros \n")
+
+conexion1.close()
+
